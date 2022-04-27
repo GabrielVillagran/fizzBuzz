9344f9988037863656aa2d2a463a618291c9edb8
gabriel <metalmadnes98@gmail.com>
2022-04-27 17:03:17 -0500

    se realizaron las pruebas y se verifico el funcionamiento de la clase reader

--- test/reader.test.js
+++ test/reader.test.js
@@ -1,17 +1,21 @@
 // clase que vamos a realizar pruebas
-const reader = require("../fizzbuzz/lib/utils/reader")
+const reader = require('../../fizzbuzz/lib/utils/reader')
 
 // pruebas para verificar la lectura de archivos
 describe("pruebas para reader.js", () => {
     // Prueba de caso 1
     test('Case1: lectura de archivos', () => {
         // codigo que se desea proba
-        const file = reader.readFile("explorers.json")
+        const fileReader = reader.readFile("explorers.json")
+        console.log(fileReader)
 
-        // Resultado que se espera de las pruebas
-        expect(file.name).toBe("woopa1");
-        expect(file.githubUsername).toBe("ajolonauta1");
-        // expect(user.name).toBe("Gabriel");
-        // expect(user.bio).not.toBeUndefined();
+        // Resultado que se espera de las pruebas Ajolonauta1
+        expect(fileReader[0].name).toBe("Woopa1");
+        expect(fileReader[0].githubUsername).toBe("ajolonauta1");
+        expect(fileReader[0].score).toBe(1);
+        // ajolonauta 15
+        expect(fileReader[14].name).toBe("Woopa15");
+        expect(fileReader[14].githubUsername).toBe("ajolonauta15");
+        expect(fileReader[14].score).toBe(15);
     });
 })
